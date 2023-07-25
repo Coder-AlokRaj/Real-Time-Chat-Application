23ff8a80d4c56cb279ebb4f51a837dbe7eefa84e
Coder-AlokRaj <alokraj.ias3@gmail.com>
2023-07-25 10:37:12 +0530

    update to original

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Chat from "./Chat";
 
 
-const socket = io.connect("https://alok-chat-application.onrender.com");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const io = new Server(server, {
 
 
 
+
   io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
   
